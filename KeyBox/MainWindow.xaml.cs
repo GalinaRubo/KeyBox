5b55a2a56bafe829d0988aa53cb850d6548f8bc0
Galina.Rubo <Rubo_eh74@itstepru.onmicrosoft.com>
2022-08-29 18:16:59 +0700

    Добавьте файлы проекта.

---
+++ KeyBox/MainWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyBox
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Box_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Esc_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ё_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Два_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Три_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Четыре_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Шесть_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Семь_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Tab_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Й_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ц_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void У_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void К_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Е_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Г_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ш_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Caps_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ф_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ы_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void В_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void А_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void П_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void О_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Л_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Shift_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Я_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ч_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void С_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void М_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void И_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ь_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Б_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Ctrl_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void W_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Alt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Пробел_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void _Alt_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void w_Click_1(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void _Ctrl_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Cтрелка_влево_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Cтрелка_вниз_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Н_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Р_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Т_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Один_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
